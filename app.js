3031fbfa884607f454feb52e0e8500ce465141d4
M MUKIT <imukit007@gmail.com>
2016-04-03 10:28:23 +0000

    Add backend processing

---
+++ app.js
@@ -0,0 +1,34 @@
+var moment = require("moment");
+var express = require("express");
+var app = express();
+
+app.get("/:data", function(req, res){
+  var data = req.params.data;
+  var myDate;
+  if(/^\d{8,}$/.test(data)) {
+    myDate = moment(data, "X");
+  } else {
+    myDate = moment(data, "MMMM D, YYYY");
+  }
+  
+  if(myDate.isValid()) {
+    res.json({
+      unix: myDate.format("X"),
+      natural: myDate.format("MMMM D, YYYY")
+    });
+  } else {
+    res.json({
+      unix: null,
+      natural: null
+    });
+  }
+});
+
+app.listen(process.env.PORT, process.env.IP, function(err){
+  if (err) {
+    throw err;
+  } else {
+    console.log("Server Started");
+  }
+  
+});
